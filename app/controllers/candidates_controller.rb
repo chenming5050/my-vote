c58ccd3a0d55c8c2ba1a1907fd739fd171a76574
chenming5050 <chenming5050@gmail.com>
2017-04-28 00:43:56 -0400

    first commit

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,61 @@
+class CandidatesController < ApplicationController
+  before_action :find_candidate, only: [:show, :edit, :update, :destroy, :vote]
+  
+  def index
+    @candidates = Candidate.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @candidate = Candidate.new
+  end
+  
+  def create
+    @candidate = Candidate.new(candidate_params)
+    
+    if @candidate.save
+      redirect_to candidates_path, notice: "新增完成"
+    else
+      render 'new'
+#       redirect_to new_candidate_path
+      
+    end
+  end
+  
+  def edit
+  end
+  
+  def update    
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "update"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @candidate.destroy
+    #flash[:notice] = "deleted!"
+    redirect_to candidates_path, notice: "deleted!"
+  end
+  
+  def vote
+#     @candidate.vote
+    log = Log.new(candidate: @candidate, ip_address: request.remote_ip)
+    @candidate.logs << log 
+    @candidate.save
+    redirect_to candidates_path, notice: "done!"
+  end
+  
+  private
+  def candidate_params
+    params.require("candidate").permit(:name, :party, :age, :politics)
+  end
+  
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[ :id])
+    redirect_to candidates_path, notice: "no data!" if @candidate.nil?
+  end
+end
